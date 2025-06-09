2a9c8ed24662393474d7a288f1c0e252f1d2062d
AsoreFox <114624068+AsoreFox@users.noreply.github.com>
2025-06-09 03:06:07 -0300

    Added some comments and moved the code for estetics reasons

--- tests/ui/test_login_ui.py
+++ tests/ui/test_login_ui.py
@@ -8,22 +8,17 @@ from pages.logged_page import LoggedInPage
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 
-@pytest.fixture
+@pytest.fixture #loads config file with test data
 def config():
     with open(os.path.join("config", "config.json")) as  f:
         return json.load(f)
     homePage = HomePage(driver)
     homePage.visit(config["base_url"])
-@pytest.fixture
+@pytest.fixture # opnes pages an initialitates the instance of HomePage
 def home_page(driver, config):
     homePage = HomePage(driver)
     homePage.visit(config["base_url"])
     return homePage
 
-
-
-
-
 def test_singup(driver,config, home_page):
     home_page.verify_home_page
-
